0fdbef9e7105f3242fdbeaa4331155b10b200b56
hxunz <guswl2843@gmail.com>
2022-04-18 23:52:08 +0900

    ✨ routine 생성 , routine list 구현

--- src/pages/DailyRoutine/DailyRoutine.tsx
+++ src/pages/DailyRoutine/DailyRoutine.tsx
@@ -1,20 +1,45 @@
-import Input from "./Input"
-
-function List() {
-  return null
-}
+import { useState } from 'react';
 
 const DailyRoutine: React.VFC = () => {
-  function onClick() {
-    //Todo: 할 일 추가 
+  const [routine, setRoutine] = useState('');
+  const [tasks, setTasks] = useState<{id: number, title: string}[]>([]);
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setRoutine(event.target.value);
   }
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+    if (!routine) {
+      return;
+    }
+
+    setTasks((prevState) => [
+      ...prevState,
+      {
+        id: prevState.length,
+        title: routine
+      }
+    ]);
+    setRoutine('');
+
+    event.preventDefault();
+  }
+
   return (
     <div>
       <h1>
        Make your routine
       </h1>
-      <Input onClick={onClick}/>
-      <List />
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="write" value={routine} onChange={handleChange}/>
+        <input type="submit" value="add" />
+      </form>
+
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.title}</li>
+        ))}
+      </ul>
     </div>
   )
 }

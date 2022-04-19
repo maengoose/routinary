250b44d2509d42cec3edb045c4ff8cb591c089a3
hxunz <guswl2843@gmail.com>
2022-04-19 14:07:40 +0900

    ⚡️ routine type 추가

--- src/pages/DailyRoutine/DailyRoutine.tsx
+++ src/pages/DailyRoutine/DailyRoutine.tsx
@@ -1,34 +1,42 @@
 import { useState } from 'react';
 
+type Routine = {
+  id: number;
+  title: string;
+  startTime: number;
+  time: number;
+}
+
 const DailyRoutine: React.VFC = () => {
   const [routine, setRoutine] = useState('');
-  const [tasks, setTasks] = useState<{id: number, title: string}[]>([]);
+  const [routines, setRoutines] = useState<Routine[]>([]);
 
   const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     setRoutine(event.target.value);
   }
 
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
     if (!routine) {
       return;
     }
 
-    setTasks((prevState) => [
+    setRoutines((prevState) => [
       ...prevState,
       {
         id: prevState.length,
-        title: routine
+        title: routine,
+        startTime: 20,
+        time: 2
       }
     ]);
     setRoutine('');
-
-    event.preventDefault();
   }
 
   return (
     <div>
       <h1>
-       Make your routine
+      Make your routine
       </h1>
       <form onSubmit={handleSubmit}>
         <input type="text" placeholder="write" value={routine} onChange={handleChange}/>
@@ -36,8 +44,8 @@ const DailyRoutine: React.VFC = () => {
       </form>
 
       <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>{task.title}</li>
+        {routines.map(({ id, title, startTime }) => (
+          <li key={id}>{startTime} {title}</li>
         ))}
       </ul>
     </div>

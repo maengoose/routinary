a539fc932c7e2b739e77b7751214e16cc3b9d625
hxunz <guswl2843@gmail.com>
2022-05-09 14:36:52 +0900

    ✨ home css 생성 & underline 삭제 & background 이미지 삽입

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+
+import './Home.css';
 
 const Home: React.VFC = () => {
   return (
-    <Link to='/routine'>
-      Daily Routine
-    </Link>
+    <div className='background'>
+      <Link to='/routine' className='link'>
+        <Button variant='contained'>Daily Routine</Button>
+      </Link>
+    </div>
   );
 }
 

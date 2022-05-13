549c801b2c466c9e202fa82b41b820dc6178cd0b
hxunz <guswl2843@gmail.com>
2022-05-13 18:17:05 +0900

    ✏️  fix typos

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from './pages/Home';
 import DailyRoutine from './pages/DailyRoutine';
-import Login from "./pages/Login";
+import Login from './pages/Login';
 import SideBar from "./SideBar";
 
 

1b22c11231247ff97bbc702b840f488a2218f5c9
hxunz <guswl2843@gmail.com>
2022-05-25 16:06:53 +0900

    ✨ rain sound 버튼 기능 구현

--- src/pages/Music/Music.tsx
+++ src/pages/Music/Music.tsx
@@ -1,7 +1,39 @@
+import { useEffect, useRef, useState } from 'react';
+import * as Styled from './style';
+import Rain from './WhiteNoise/Rain.mp3';
+
 const Music: React.VFC = () => {
+  // const audio = new Audio(Rain);
+
+  // const handleRainMusicButton = () => {
+  //   audio.play();
+  // }
+
+  const useAudio = (url: any) => {
+  const audio = useRef(new Audio(url));
+  const [playing, setPlaying] = useState(false);
+
+  const toggle = () => setPlaying(!playing);
+
+  useEffect(() => {
+    if (audio.current) {
+      playing ? audio.current.play() : audio.current.pause(); 
+    }
+  },
+  [playing]);
+
+  return [toggle];
+  };
+  
+  const [toggle] = useAudio(Rain);
+
   return (
-    <p>music</p>
+    <Styled.RainMusicButton
+      onClick={toggle}
+    >
+      Rain
+    </Styled.RainMusicButton>
   )
-}
+};
 
 export default Music;

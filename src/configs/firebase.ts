0d7a5ad50b7e48dec7068e53b6f2c4ab36ebeb5d
hxunz <guswl2843@gmail.com>
2022-06-14 00:02:38 +0900

    ✨ firebase messaging 연결

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getMessaging, getToken } from "firebase/messaging";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDHIcXbdTuIZGUvYXtzqNc8ewKFXoMcSeA",
   authDomain: "routinary-1322c.firebaseapp.com",
@@ -16,6 +12,17 @@ const firebaseConfig = {
   measurementId: "G-8FENK06030"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const messaging = getMessaging(app);
+
+getToken(messaging, { vapidKey: 'BMv6gULZnhzKgDyb7TiQ59kGDS3EiTUNo7VnGoeEDODg55Yq2AW_ee6cLLsbZ09ZECa8OTJjE-CrsNE1NbvCG2E' })
+  .then((currentToken) => {
+    if (currentToken) {
+      console.log('currentToken', currentToken);
+    } else {
+      console.log('No registration token available. Request permission to generate one.');
+    }
+  }).catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+  });

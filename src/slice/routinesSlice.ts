7e89e13c6caa347b2c677f9729f888f546c219e7
hxunz <guswl2843@gmail.com>
2022-08-07 17:14:39 +0900

    ✏️ Fix typos

--- src/slice/routinesSlice.ts
+++ src/slice/routinesSlice.ts
@@ -1,5 +1,4 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { useCallback } from 'react';
 // import type { PayloadAction } from '@reduxjs/toolkit'
 // import type { RootState } from '../store'
 import { Routine } from '../pages/DailyRoutines/DailyRoutines'
